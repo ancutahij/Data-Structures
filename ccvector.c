7e324ebcf72c504c2f985b87e6cc9a9abc479b08
Ancuta <ancutahij@gmail.com>
2018-11-18 12:51:34 +0200

    Dynamic vector features added

--- ccvector.c
+++ ccvector.c
@@ -7,20 +7,24 @@
 int VecCreate(CC_VECTOR **Vector)
 {
 	// Vector parameter has already an address memory.
-    if (*Vector != NULL){
+    if (*Vector != NULL)
+	{
 		return -1;
 	}
 
 	*Vector = malloc(sizeof(CC_VECTOR));
-	if (Vector == NULL) {
+	if (Vector == NULL) 
+	{
 		return -1;
 	}
 
 	(*Vector)->memorySize = VEC_DEFAULT_SIZE;
 	(*Vector)->actualSize = 0;
 	(*Vector)->elements = malloc(sizeof(int) * VEC_DEFAULT_SIZE);
-	if ((*Vector)->elements == NULL) {
+	if ((*Vector)->elements == NULL)
+	{
 		free(Vector);
+		*Vector = NULL;
 		return -1;
 	}
 	return 0;
@@ -28,11 +32,13 @@ int VecCreate(CC_VECTOR **Vector)
 
 int VecDestroy(CC_VECTOR **Vector)
 {
-	if (Vector == NULL) {
+	if (Vector == NULL) 
+	{
 		return -1;
 	}
 
-	if (*Vector == NULL) {
+	if (*Vector == NULL)
+	{
 		return -1;
 	}
 	free((*Vector)->elements);
@@ -44,15 +50,18 @@ int VecDestroy(CC_VECTOR **Vector)
 
 int VecInsertTail(CC_VECTOR *Vector, int Value)
 {
-	if (Vector == NULL ) {
+	if (Vector == NULL || Vector->elements == NULL ) 
+	{
 		return -1;
 	}
 	
 	int currentSize = Vector->actualSize;
 
-	if (currentSize== Vector->memorySize) {
+	if (currentSize== Vector->memorySize) 
+	{
 		int *temp = realloc(Vector->elements, currentSize * 2 * sizeof(int));
-		if (temp == NULL) {
+		if (temp == NULL)
+		{
 			return -1;
 		}
 		Vector->elements = temp;
@@ -68,24 +77,94 @@ int VecInsertTail(CC_VECTOR *Vector, int Value)
 
 int VecInsertHead(CC_VECTOR *Vector, int Value)
 {
-    CC_UNREFERENCED_PARAMETER(Vector);
-    CC_UNREFERENCED_PARAMETER(Value);
-    return -1;
+	if (Vector == NULL || Vector->elements == NULL) 
+	{
+		return -1;
+	}
+	
+	int currentSize = Vector->actualSize;
+	if (currentSize >= Vector->memorySize) {
+		int *temp = realloc(Vector->elements,( currentSize * 2 )* sizeof(int));
+		if (temp == NULL) 
+		{
+			return -1;
+		}
+		Vector->elements = temp;
+		Vector->memorySize = 2 * currentSize;
+	}
+
+	int i = currentSize;
+	int *arr = Vector->elements;
+	for (; i > 0; i--) 
+	{
+		arr[i ] = arr[i-1];
+	}
+	arr[0] = Value;
+	Vector->actualSize = currentSize + 1;
+
+    return 0;
 }
 
 int VecInsertAfterIndex(CC_VECTOR *Vector, int Index, int Value)
 {
-    CC_UNREFERENCED_PARAMETER(Vector);
-    CC_UNREFERENCED_PARAMETER(Index);
-    CC_UNREFERENCED_PARAMETER(Value);
-    return -1;
+	// in case that the Vector pointer points to a NULL adddress or the vector of elements points to a NULL address
+	if (Vector == NULL || Vector->elements==NULL) 
+	{
+		return -1;
+	}
+	int currentSize = Vector->actualSize;
+	if (currentSize >= Vector->memorySize)
+	{
+		int *temp = realloc(Vector->elements, (currentSize * 2) * sizeof(int));
+		if (temp == NULL)
+		{
+			return -1;
+		}
+		Vector->elements = temp;
+		Vector->memorySize = 2 * currentSize;
+	}
+
+
+	int i = currentSize;
+	int *arr = Vector->elements;
+
+	for (; i > Index+1 ; i--) 
+	{
+		arr[i] = arr[i - 1];
+
+	}
+	arr[Index+1] = Value;
+	Vector->actualSize = currentSize + 1;
+
+    return 0;
 }
 
 int VecRemoveByIndex(CC_VECTOR *Vector, int Index)
 {
-    CC_UNREFERENCED_PARAMETER(Vector);
-    CC_UNREFERENCED_PARAMETER(Index);
-    return -1;
+	if (Vector == NULL || Vector->elements == NULL)
+	{
+		return-1;
+	}
+
+	int i = Index;
+	int currentSize = Vector->actualSize;
+	int *arr = Vector->elements;
+	for (; i < currentSize; i++)
+	{
+		arr[i] = arr[i + 1];
+	}
+	Vector->actualSize = currentSize - 1;
+
+	if (Vector->actualSize <= (Vector->memorySize / 4))
+	{
+		int *temp = realloc(Vector->elements, sizeof(int) * (Vector->memorySize / 2));
+		if (temp == NULL)
+		{
+			return -1;
+		}
+		Vector->elements = temp;
+	}
+    return 0;
 }
 
 int VecGetValueByIndex(CC_VECTOR *Vector, int Index, int *Value)
